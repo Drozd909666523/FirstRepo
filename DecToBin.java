cb8fa08256061585c99978c3a981d425c0868949
Drozd909666523 <drozdow.ivan2018@yandex.ru>
2024-09-19 01:06:39 +0300

    Add DecToBi.java

---
+++ DecToBin.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DecToBin {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int num = parseInt(scanner.nextLine());
+        
+        System.out.println(num);
+        
+    }
+}
